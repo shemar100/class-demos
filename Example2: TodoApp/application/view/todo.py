60137bd6ee018a2ecac1b0d8249349ca148a0616
shemar100 <anderson.shemar17@gmail.com>
2021-09-26 02:15:07 -0500

    Added session to controller

--- Example2: TodoApp/application/view/todo.py
+++ Example2: TodoApp/application/view/todo.py
@@ -1,7 +1,8 @@
 from application import app, db
 from flask import render_template, request, redirect, url_for, jsonify
 from application.model import Todo
-
+#optional import of sys lib
+import sys
 
 @app.route('/')
 def index():
@@ -10,10 +11,19 @@ def index():
 
 @app.route('/todos/create', methods=['POST'])
 def todos_create():
-    description = request.get_json()['description']
-    todo = Todo(description=description)
-    db.session.add(todo)
-    db.session.commit()
-    return jsonify({
-        'description' : todo.description
-    })
+    error = False
+    body = {}
+    try:
+        description = request.get_json()['description']
+        todo = Todo(description=description)
+        db.session.add(todo)
+        db.session.commit()
+        body['description'] = todo.description
+    except:
+        error = True #Setting error to true if there is an exception running application
+        db.session.rollback()
+        print(sys.exc_info())
+    finally:
+        db.session.close()
+    if not error:
+        return jsonify(body)
