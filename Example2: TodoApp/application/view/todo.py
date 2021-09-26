f8664a124bb8e84f23544232d4ef31512783dd41
shemar100 <anderson.shemar17@gmail.com>
2021-09-25 23:09:03 -0500

    Implemented create todo view and route handler.

--- Example2: TodoApp/application/view/todo.py
+++ Example2: TodoApp/application/view/todo.py
@@ -1,5 +1,5 @@
-from application import app
-from flask import render_template
+from application import app, db
+from flask import render_template, request, redirect, url_for
 from application.model import Todo
 
 
@@ -7,3 +7,11 @@ from application.model import Todo
 def index():
     todos = Todo.query.all()
     return render_template('index.html', data=todos)
+
+@app.route('/todos/create', methods=['POST'])
+def todos_create():
+    description = request.form.get('description', '')
+    todo = Todo(description=description)
+    db.session.add(todo)
+    db.session.commit()
+    return redirect(url_for('index'))
