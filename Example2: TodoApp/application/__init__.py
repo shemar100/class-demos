b0eb5ae3eaba2f8c89b5b151f6af383bebf85da1
shemar100 <anderson.shemar17@gmail.com>
2021-09-26 04:32:33 -0500

    database migration

--- Example2: TodoApp/application/__init__.py
+++ Example2: TodoApp/application/__init__.py
@@ -1,10 +1,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Hotskull!000@localhost:5432/todoapp'
 db = SQLAlchemy(app)
 
+migrate = Migrate(app, db)
+
 import application.view.todo
 
-db.create_all()
+#db.create_all()
--- Example2: TodoApp/application/model.py
+++ Example2: TodoApp/application/model.py
@@ -3,6 +3,7 @@ from application import db
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(), nullable=False)
-
+    completed = db.Column(db.Boolean, nullable=False, default=False)
+    
     def __repr__(self):
         return f'<Todo ID: {self.id}, description: {self.description}>'
