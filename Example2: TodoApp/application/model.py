8e172175e1d11b82c5abf7d32ee877d7b96e4c4c
shemar100 <anderson.shemar17@gmail.com>
2021-09-28 23:53:06 -0500

    Removed cards and added data model and view representation for todolist

--- Example2: TodoApp/application/model.py
+++ Example2: TodoApp/application/model.py
@@ -1,7 +1,10 @@
 from os import name
+
+from sqlalchemy.sql.expression import false
 from application import db
 
 class Todo(db.Model):
+    __tablename__ = 'todo'
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(), nullable=False)
     completed = db.Column(db.Boolean, nullable=False, default=False)
@@ -9,9 +12,10 @@ class Todo(db.Model):
 
 #This is the parent model
 class TodoList(db.Model):
+    __tablename__ = 'todolist'
     id = db.Column(db.Integer(), primary_key=True)
     name = db.Column(db.String(50), nullable=False)
     todos = db.relationship('Todo', backref='list', lazy=True)
     
     def __repr__(self):
-        return f'<Todo ID: {self.id}, description: {self.description}>'
+        return f'<Todo ID: {self.id}, name: {self.name}>'
--- Example2: TodoApp/application/view/todo.py
+++ Example2: TodoApp/application/view/todo.py
@@ -1,13 +1,8 @@
 from application import app, db
 from flask import render_template, request, redirect, url_for, jsonify
-from application.model import Todo
+from application.model import Todo, TodoList
 #optional import of sys lib
 import sys
-
-@app.route('/')
-def index():
-    todos = Todo.query.order_by('id').all()
-    return render_template('index.html', data=todos)
 
 @app.route('/todos/create', methods=['POST'])
 def todos_create():
@@ -49,3 +44,15 @@ def delete_todos(todo_id):
     return jsonify({
         'success' : True
     })
+
+
+@app.route('/list/<list_id>')
+def get_list_todos(list_id):
+    list = TodoList.query.all()
+    todos = Todo.query.filter_by(list_id=list_id).order_by('id').all()
+    return render_template('index.html', todos=todos, list=list)
+
+#redirects home route to get_list_todos route eg. list/num
+@app.route('/')
+def index():
+    return redirect(url_for('get_list_todos', list_id=1))
